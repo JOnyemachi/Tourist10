7045ffaa685f3f46c77dfc59effa66329b8ed741
Jeff Onyemachi <60332952+JOnyemachi@users.noreply.github.com>
2023-09-21 23:58:37 -0500

    updated README

--- touristten/src/App.jsx
+++ touristten/src/App.jsx
@@ -6,52 +6,52 @@ const App = (props) => {
   const[cards]  = useState ([
     {
       title: 'Hong Kong',
-      text: 'Visitors annually: 26.6',
+      text: 'Visitors annually: 26.6M',
       img: "/hong kong.jpeg"
     },
     {
       title: 'Bangkok',
-      text: 'Visitors annually: 21.2',
+      text: 'Visitors annually: 21.2M',
       img: "/bangkok.webp"
     },
     {
       title: 'London',
-      text: 'Visitors annually: 19.2',
+      text: 'Visitors annually: 19.2M',
       img: "/london.jpeg"
     },
     {
       title: 'Singapore',
-      text: 'Visitors annually: 16.6',
+      text: 'Visitors annually: 16.6M',
       img: "/singapore.webp"
     },
     {
       title: 'Macau',
-      text: 'Visitors annually: 15.4',
+      text: 'Visitors annually: 15.4M',
       img: "/macau.jpeg"
     },
     {
       title: 'Dubai',
-      text: 'Visitors annually: 14.9',
+      text: 'Visitors annually: 14.9M',
       img: "/dubai.jpeg"
     },
     {
       title: 'Paris',
-      text: 'Visitors annually: 14.4',
+      text: 'Visitors annually: 14.4M',
       img: "/paris.jpeg"
     },
     {
       title: 'New York',
-      text: 'Visitors annually: 12.7',
+      text: 'Visitors annually: 12.7M',
       img: "/newyork.avif"
     },
     {
       title: 'Shenzhen',
-      text: 'Visitors annually: 12.6',
+      text: 'Visitors annually: 12.6M',
       img: "/shenzhen.avif"
     },
     {
       title: 'Kuala Lumpur',
-      text: 'Visitors annually: 12.3',
+      text: 'Visitors annually: 12.3M',
       img: "/kuala.jpeg"
     },
   ])
